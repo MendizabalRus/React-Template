b566e807fe903f51c68a3bd90ba26c9408209f88
Mendizabal <rusmz@Mendizabals-MacBook-Air.local>
2026-05-30 00:44:51 +0200

    Subtle Fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import './main.css'
+import './reset.css'
+import './reset.css'
+
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
