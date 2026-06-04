8fc2012a814125d11aa73d4618738ccdf94dc0a7
Mendizabal <rusmz@Mendizabals-MacBook-Air.local>
2026-06-04 13:50:15 +0200

    added style and tests changes

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -2,10 +2,8 @@ import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { App } from '../App';
 
-describe('App renders correctly', () => {
-    test("heding renders correctly", () => {
-        render(<App/>);
-        const heading = screen.getByText("Hello");
-        expect(heading).toBeInTheDocument();
+describe("", () => {
+    test("", () => {
+        
     });
 });
