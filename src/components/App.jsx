98eb9313c30691f9694320fdab905dde9f3e235a
Mendizabal <rusmz@Mendizabals-MacBook-Air.local>
2026-06-06 21:35:46 +0200

    added prettier and reorganised ./src

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
+import '../style/App.css';
 
 export function App() {
 
 
   return (
     <>
-      <h1>Hello</h1>
+      
     </>
-  )
+  );
 }
--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,9 +1,7 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import { App } from '../App';
+import { App } from '../components/App';
 
-describe("", () => {
-    test("", () => {
-        
-    });
+describe('', () => {
+  test('', () => {});
 });
